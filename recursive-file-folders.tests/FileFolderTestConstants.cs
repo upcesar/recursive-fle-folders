29fcf062c549a9d555a23de0996a2e7f355464a5
Cesar M. Urdaneta <upcesar@gmail.com>
2023-02-23 23:24:37 -0300

    Added constants

--- recursive-file-folders.tests/FileFolderTestConstants.cs
+++ recursive-file-folders.tests/FileFolderTestConstants.cs
@@ -2,10 +2,11 @@ using System.IO;
 
 namespace recursive_file_folders.tests;
 
-public static class FileFolderTestConstants 
+public static class FileFolderTestConstants
 {
     public const int MaxFolderDepth = 4;
     public const int MaxFiles = 4;
-    public const string RootDirectoryName = "testRootDir";    
+    public const string RootDirectoryName = "testRootDir";
+    public const string TestSubDirectoryName = "subdir-test";
     public static string CurrentDirectory => Directory.GetCurrentDirectory();
 }
